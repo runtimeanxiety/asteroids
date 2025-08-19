edf33779f9ff7ddd28f30d8186da3e8ebf7503b0
runtimeanxiety <winkie.storm@gmail.com>
2025-08-19 21:50:59 +0800

    added collision between bullet and asteroids

--- main.py
+++ main.py
@@ -45,6 +45,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
 
 if __name__ == "__main__":
