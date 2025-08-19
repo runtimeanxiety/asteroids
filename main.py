82b806e58e55d8d9be4617c2868591f9b0069669
runtimeanxiety <winkie.storm@gmail.com>
2025-08-19 17:21:00 +0800

    optimized resources

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    FPS = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, "black")
         pygame.display.flip()
+        dt = FPS.tick(60) / 1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
